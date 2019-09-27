3eefdd0323a97326595964ad5b26b8a6a8ce664a
nitin <nitin@192.168.1.68>
2019-09-27 16:52:23 -0700

    Added User Data for creating EC2 Instance

--- FirstJS/launch-ec2.js
+++ FirstJS/launch-ec2.js
@@ -21,13 +21,27 @@ AWS.config.getCredentials(function(err) {
 var ec2 = new AWS.EC2({apiVersion: '2016-11-15'});
 
 // AMI is ami-47d7752c is Backspace Academy (Udemy) Node JS LINUX Image
+/**
+ * User Data below is following commands encoded in BASE64 (required by AWS
+ * #!/bin/bash
+ * yum -y update
+ * iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8080
+ * iptables -A INPUT -p tcp -m tcp --sport 80 -j ACCEPT
+ * iptables -A OUTPUT -p tcp -m tcp --dport 80 -j ACCEPT
+ * 
+ * This is required because EC2 does not provide access to ports below 1000, and therefore
+ * Node JS has to run on 8080, and HTTP requests (port 80) are routed to 8080
+ */
+
+
 var instanceParams = {
    ImageId: 'ami-47d7752c', 
    InstanceType: 't1.micro',
    KeyName: 'ec2-test',
    MinCount: 1,
    MaxCount: 1,
-   SecurityGroups: ['webAccess']
+   SecurityGroups: ['webAccess'],
+   UserData: "IyEvYmluL2Jhc2gKaXB0YWJsZXMgLUEgUFJFUk9VVElORyAtdCBuYXQgLWkgZXRoMCAtcCB0Y3AgLS1kcG9ydCA4MCAtaiBSRURJUkVDVCAtLXRvLXBvcnQgODA4MAppcHRhYmxlcyAtQSBJTlBVVCAtcCB0Y3AgLW0gdGNwIC0tc3BvcnQgODAgLWogQUNDRVBUCmlwdGFibGVzIC1BIE9VVFBVVCAtcCB0Y3AgLW0gdGNwIC0tZHBvcnQgODAgLWogQUNDRVBU"
 };
 
 // Create a promise on an EC2 service object
